4b09e56306cf81d5a40ed8063486e7245881a392
MikkelHebel <mikkelhhebel@gmail.com>
2024-01-23 23:03:10 +0100

    Implemented login functionality

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import { connectToDatabase } from '@utils/database';
+import User from '@models/user';
 
 const handler = NextAuth({
     providers: [
@@ -9,15 +11,36 @@ const handler = NextAuth({
         })
     ],
 
-    async session({ session }) {
-        
-    },
-
-    async signIn({ profile }) {
-        try {
-
-        } catch (error) {
-            console.log(error);
+    callbacks: {
+        async session({ session }) {
+            const userSession = await User.findOne({ email: session.user.email });
+            session.user.id = userSession._id.toString();
+    
+            return session;
+        },
+    
+        async signIn({ profile }) {
+            try {
+                await connectToDatabase();
+    
+                // Check if user exists
+                const userExists = await User.findOne({ email: profile.email });
+    
+                // If user !exists, create user
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", ""),
+                        image: profile.picture
+                    });
+    
+                    await user.save();
+                }
+    
+                return true;
+            } catch (error) {
+                console.log(error);
+            }
         }
     }
 })
