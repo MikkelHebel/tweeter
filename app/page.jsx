d7c3cb6848fb177c2fff90cb10ebdbd567fda060
MikkelHebel <mikkelhhebel@gmail.com>
2024-01-23 02:45:32 +0100

    Made some of the login and homepage

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import Feed from '@components/Feed';
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">Tweeter
+            <br className="max-md:hidden" />
+            <span className="orange_gradient text-center">a project for sending tweets, just like twitter!</span>
+        </h1>
+        <p className="desc text-center">This is a next.js project where I have implemented create, read, update, delete (CRUD) capability.</p>
+
+        <Feed />
+    </section>
+  )
+}
+
+export default Home
---
+++ components/Nav.jsx
@@ -0,0 +1,94 @@
+"use client";
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
+
+const Nav = () => {
+  const isUserLoggedIn = true;
+  const [providers, setProviders] = useState(null);
+
+  const [toggleDropdown, setToggleDropdown] = useState(false)
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    }
+
+    setProviders();
+  }, [])
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+        <Link href="/" className="flex gap-2 flex-center">
+            <Image src="/assets/images/logo.svg" className="object-contain" width={50} height={50} alt="Tweeter logo" />
+            <p className="logo_text">Tweeter</p>
+        </Link>
+
+        {/* Desktop Nav */}
+        <div className="sm:flex hidden">
+            {isUserLoggedIn ? (
+                <div className="flex gap-3 md:gap-5">
+                    <Link href="/create-prompt" className="black_btn">
+                        Post tweet
+                    </Link>
+
+                    <button type="button" onClick={signOut} className="outline_btn">Sign Out</button>
+
+                    <Link href="/profile" className="flex gap-2 flex-center">
+                        <Image src="/assets/images/logo.svg" className="rounded-full" width={37} height={37} alt="User avatar" />
+                    </Link>
+                </div>
+            ) : (
+                <>
+                    {providers && 
+                    Object.values(providers).map((provider) => (
+                        <button type="button" key={provider.name} onClick={() => signIn(provider.id, { callbackUrl: '/' })} className="black_btn">
+                            Sign in
+                        </button>
+                    )
+                    )}
+                </>
+            )}
+        </div>
+
+        {/* Mobile Nav */}
+        <div className="sm:hidden flex relative">
+            {isUserLoggedIn ? (
+                <div className="flex">
+                    <Image src="/assets/images/logo.svg" className="rounded-full" width={37} height={37} alt="User avatar" onClick={() => setToggleDropdown((prev) => (!prev))} />
+
+                    {toggleDropdown && (
+                        <div className="dropdown">
+                            <Link href="/profile" className="dropdown_link" onClick={() => setToggleDropdown(false)}>
+                                My Profile
+                            </Link>
+                            <Link href="/create-prompt" className="dropdown_link" onClick={() => setToggleDropdown(false)}>
+                                Post tweet
+                            </Link>
+                            <button type="button" className="mt-5 w-full black_btn" onClick={() => {setToggleDropdown(false); signOut();}}>
+                                Sign Out
+                            </button>
+                        </div>
+                    )}
+                </div>
+            ) : (
+                <>
+                    {providers && 
+                    Object.values(providers).map((provider) => (
+                        <button type="button" key={provider.name} onClick={() => signIn(provider.id, { callbackUrl: '/' })} className="black_btn">
+                            Sign in
+                        </button>
+                    )
+                    )}
+                </>
+            )}
+        </div>
+    </nav>
+  )
+}
+
+export default Nav
