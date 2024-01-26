f22887e548aefdbf1af94e7718a22de45705f53a
MikkelHebel <mikkelhhebel@gmail.com>
2024-01-26 01:29:09 +0100

    Fix another caching issue

--- app/api/posts/get/route.js
+++ app/api/posts/get/route.js
@@ -4,8 +4,9 @@ import Post from "@models/post";
 export const GET = async (req) => {
     try {
         await connectToDatabase();
-
-        const posts = await Post.find({}).populate('creator');
+        
+        // Disabling cache
+        const posts = await Post.find({}).populate('creator').cache({ ttl: 10 });
         return new Response(JSON.stringify(posts), { status: 200 });
     } catch (error) {
         console.log(error);
